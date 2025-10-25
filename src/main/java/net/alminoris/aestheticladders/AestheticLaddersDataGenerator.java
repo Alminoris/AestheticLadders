c818c628a46804fc5fa317eb8f3c73634093e617
AOA <aoa@d.co>
2025-10-25 18:01:21 +0300

    #Update 2.0.1

--- src/main/java/net/alminoris/aestheticladders/AestheticLaddersDataGenerator.java
+++ src/main/java/net/alminoris/aestheticladders/AestheticLaddersDataGenerator.java
@@ -1,9 +1,6 @@
 package net.alminoris.aestheticladders;
 
-import net.alminoris.aestheticladders.datagen.ModBlockTagProvider;
-import net.alminoris.aestheticladders.datagen.ModLootTableProvider;
-import net.alminoris.aestheticladders.datagen.ModModelProvider;
-import net.alminoris.aestheticladders.datagen.ModRecipeProvider;
+import net.alminoris.aestheticladders.datagen.*;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
@@ -18,5 +15,11 @@ public class AestheticLaddersDataGenerator implements DataGeneratorEntrypoint
 		pack.addProvider(ModModelProvider::new);
 		pack.addProvider(ModLootTableProvider::new);
 		pack.addProvider(ModBlockTagProvider::new);
+		pack.addProvider(ModLanguageProviderEnUs::new);
+		pack.addProvider(ModLanguageProviderDeDe::new);
+		pack.addProvider(ModLanguageProviderEsEs::new);
+		pack.addProvider(ModLanguageProviderFrFr::new);
+		pack.addProvider(ModLanguageProviderRuRu::new);
+		pack.addProvider(ModLanguageProviderUkUa::new);
 	}
 }
